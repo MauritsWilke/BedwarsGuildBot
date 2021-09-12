3d7947bf9a929980392a172776f9babeb75af9bf
Maurits Wilke <mauritswilke@gmail.com>
2021-09-12 16:27:23 +0200

    Final final attempt at bypass

--- src/utils/getMaps.js
+++ src/utils/getMaps.js
@@ -31,7 +31,15 @@ const getText = async (URL) => {
 		headers: {
 			'Content-Type': 'text/html',
 			'User-Agent': 'Mozilla/5.0 Firefox/91.0',
-			'X-Forwarded-For': `203.0.113.195, 70.41.3.18, 150.172.238.178`
+			'Host': "hypixel.net",
+			'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0",
+			'Accept': `*/*`,
+			"Accept-Language": "en-GB,en;q=0.5",
+			"Accept-Encoding": "gzip, deflate, br",
+			"Referer": "https://hyapi.tech/",
+			"Origin": "https://hyapi.tech",
+			"DNT": 1,
+			"Connection": "keep-alive",
 		}
 	})
 	const text = await res.text()
